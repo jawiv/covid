bf1ff1d97a87913981d9179dde97a22898c5894a
James Webb <jawiv@github.com>
2020-10-28 13:45:46 -0500

    Update all files prior to system update

--- .ipynb_checkpoints/update_data-checkpoint.py
+++ .ipynb_checkpoints/update_data-checkpoint.py
@@ -3,12 +3,12 @@
 import requests
 import git
 
-base_url = "https://covidtracking.com/"
+base_url = "https://api.covidtracking.com/"
 
-for endpoint, file in [("api/v1/states/current.json", "states_current.json"),
-                       ("api/v1/states/daily.json", "states_history.json"),
-                       ("api/v1/us/current.json", "us_current.json"),
-                       ("api/us/daily", "us_history.json"),
+for endpoint, file in [("v1/states/current.json", "states_current.json"),
+                       ("v1/states/daily.json", "states_history.json"),
+                       ("v1/us/current.json", "us_current.json"),
+                       ("v1/us/daily.json", "us_history.json"),
                       ]:
     url = base_url + endpoint
     r = requests.get(url)
